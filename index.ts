5e4e2fbbfb1cdc9cad8c39dea4de44eef0a42a57
Rownak Abdullah <rownakabdullahomi@gmail.com>
2025-04-12 06:23:02 +0600

    add money req done

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 import { Request, Response } from "express";
 
@@ -11,14 +11,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-
-
-
-
-
-
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.d3h8n.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,7 +19,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -35,51 +27,34 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-
     const userCollection = client.db("bd_cash").collection("users");
-
-
+    const transactionCollection = client.db("bd_cash").collection("transactions");
 
     // Users APIs
     app.post("/users", async (req: Request, res: Response) => {
-        // console.log(req.body);
+      // console.log(req.body);
 
-        const query = { email: req.body.email };
-        const existingUser = await userCollection.findOne(query);
-        if (!existingUser) {
-            // Insert new user into the database
-            const result = await userCollection.insertOne(req.body);
-            res.send(result);
-        }
-        else {
-            res.send({ message: "User already exists in the database" });
-        }
-    })
+      const query = { email: req.body.email };
+      const existingUser = await userCollection.findOne(query);
+      if (!existingUser) {
+        // Insert new user into the database
+        const result = await userCollection.insertOne(req.body);
+        res.send(result);
+      } else {
+        res.send({ message: "User already exists in the database" });
+      }
+    });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    app.post("/add-money-request", async (req: Request, res: Response) => {
+      const result = await transactionCollection.insertOne(req.body);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -87,20 +62,8 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-
-
-
-
-
-
-
-
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Node + TypeScript Server is running!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Node + TypeScript Server is running!");
 });
 
 app.listen(port, () => {
